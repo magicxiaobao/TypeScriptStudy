9bb0845316c849002137ec36a8662af9a704b248
chenyuejun <rokey.yj.chen@gmail.com>
2019-03-19 00:06:50 +0800

    基础类型学习

--- build/BaseType.js
+++ build/BaseType.js
@@ -10,4 +10,50 @@ var strings = ['aa', 'bb', 'cc'];
 console.log(strings);
 var nums = [1, 2, 3];
 console.log(nums);
+// 元祖
+var meta;
+meta = ['aa', 1];
+console.log(meta[0]);
+// 枚举
+var Sex;
+(function (Sex) {
+    Sex[Sex["Male"] = 0] = "Male";
+    Sex[Sex["Female"] = 1] = "Female";
+})(Sex || (Sex = {}));
+var s = Sex.Male;
+var f = Sex.Female;
+console.log(s);
+console.log(f);
+// void
+function fc() {
+    console.log('void with no return result');
+}
+fc();
+// any的用法
+var anyArray = [1, '1'];
+console.log(anyArray);
+// null 和 undefined
+var x;
+x = null;
+// x = undefined;
+console.log(x);
+var ss;
+ss = 'a';
+ss = null;
+ss = undefined;
+// never
+var ne;
+var y;
+ne = (function () { throw new Error('aa'); })();
+// ne = 3
+y = (function () { throw new Error('aa'); })();
+function throwNever() {
+    throw new Error('error');
+}
+throwNever();
+function deadCycle() {
+    while (true) {
+    }
+}
+deadCycle();
 //# sourceMappingURL=BaseType.js.map
--- src/script/BaseType.ts
+++ src/script/BaseType.ts
@@ -12,3 +12,54 @@ let strings: string[] = ['aa', 'bb', 'cc'];
 console.log(strings);
 let nums: Array<number> = [1, 2, 3];
 console.log(nums);
+
+// 元祖
+let meta: [string, number];
+meta = ['aa', 1];
+console.log(meta[0]);
+
+// 枚举
+enum Sex {Male, Female}
+let s: Sex = Sex.Male;
+let f: Sex = Sex.Female;
+console.log(s);
+console.log(f);
+
+// void
+function fc(): void {
+    console.log('void with no return result');
+}
+fc();
+
+// any的用法
+let anyArray: any[] = [1, '1'];
+console.log(anyArray);
+
+// null 和 undefined
+let x: number;
+x = null;
+// x = undefined;
+console.log(x);
+
+let ss: string | null | undefined;
+ss = 'a';
+ss = null;
+ss = undefined;
+
+// never
+let ne: never;
+let y: number;
+ne = (() => { throw new Error('aa'); })();
+// ne = 3
+y = (() => { throw new Error('aa'); })();
+function throwNever(): never {
+    throw new Error('error');
+}
+throwNever();
+function deadCycle(): never {
+    while (true) {
+
+    }
+}
+
+deadCycle();
